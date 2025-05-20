23725e42b32b32d0eb3193b786e3c9ef958b733e
Drahiri <kodzidg@o2.pl>
2025-05-20 14:25:09 +0200

    Add BFS search algorithm

--- Chapter04/include/search.h
+++ Chapter04/include/search.h
@@ -1,6 +1,8 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include <queue>
+#include <unordered_map>
 #include <vector>
 
 struct GraphNode {
@@ -26,5 +28,42 @@ struct WeightedGraphNode {
     // Stores outgoing edges
     std::vector<WeightedEdge*> edges;
 };
+
+using NodeToParentMap = std::unordered_map<const GraphNode*, const GraphNode*>;
+
+bool BFS(
+      const Graph& graph, const GraphNode* start, const GraphNode* goal, NodeToParentMap& outMap) {
+    // Whether we found a path
+    bool pathFound = false;
+
+    // Nodes to consider
+    std::queue<const GraphNode*> q;
+
+    // Enqueue the first node
+    q.emplace(start);
+    while(!q.empty()) {
+        // Dequeue a node
+        const GraphNode* current = q.front();
+        q.pop();
+        if(current == goal) {
+            pathFound = true;
+            break;
+        }
+
+        // Enqueue adjacent nodes that aren't already on map
+        for(const GraphNode* node: current->adjacent) {
+            // If the parent is null, it hasn't been enqueued
+            // (except for the start node)
+            const GraphNode* parent = outMap[node];
+            if(parent == nullptr && node != start) {
+                // Enqueue this node, setting its parent
+                outMap[node] = current;
+                q.emplace(node);
+            }
+        }
+    }
+
+    return pathFound;
+}
 
 #endif
