b3e4ad8b3b6d191e4ef8da0c9cf6b606850f9878
Drahiri <kodzidg@o2.pl>
2025-05-20 12:50:12 +0200

    Add AIPatrol, AIDeath, AIAttack states

--- Chapter04/include/ai_state.h
+++ Chapter04/include/ai_state.h
@@ -19,4 +19,40 @@ protected:
     AIComponent* owner;
 };
 
+class AIPatrol : public AIState {
+public:
+    AIPatrol(AIComponent* owner);
+
+    // Override the behaviors for this state
+    void update(float deltaTime) override;
+    void onEnter() override;
+    void onExit() override;
+
+    const char* getName() const override;
+};
+
+class AIDeath : public AIState {
+public:
+    AIDeath(AIComponent* owner);
+
+    // Override the behaviors for this state
+    void update(float deltaTime) override;
+    void onEnter() override;
+    void onExit() override;
+
+    const char* getName() const override;
+};
+
+class AIAttack : public AIState {
+public:
+    AIAttack(AIComponent* owner);
+
+    // Override the behaviors for this state
+    void update(float deltaTime) override;
+    void onEnter() override;
+    void onExit() override;
+
+    const char* getName() const override;
+};
+
 #endif
--- Chapter04/src/ai_state.cpp
+++ Chapter04/src/ai_state.cpp
@@ -1,3 +1,65 @@
 #include "ai_state.h"
 
+#include "ai_component.h"
+
+#include <SDL3/SDL_log.h>
+
 AIState::AIState(AIComponent* owner) : owner(owner) {}
+
+AIPatrol::AIPatrol(AIComponent* owner) : AIState(owner) {}
+
+void AIPatrol::update(float deltaTime) {
+    SDL_Log("Updating %s state", getName());
+    bool dead = true;
+    if(dead) {
+        owner->changeState("Death");
+    }
+}
+
+void AIPatrol::onEnter() {
+    SDL_Log("Entering %s state", getName());
+}
+
+void AIPatrol::onExit() {
+    SDL_Log("Exiting %s state", getName());
+}
+
+const char* AIPatrol::getName() const {
+    return "Patrol";
+}
+
+AIDeath::AIDeath(AIComponent* owner) : AIState(owner) {}
+
+void AIDeath::update(float deltaTime) {
+    SDL_Log("Updating %s state", getName());
+}
+
+void AIDeath::onEnter() {
+    SDL_Log("Entering %s state", getName());
+}
+
+void AIDeath::onExit() {
+    SDL_Log("Exiting %s state", getName());
+}
+
+const char* AIDeath::getName() const {
+    return "Death";
+}
+
+AIAttack::AIAttack(AIComponent* owner) : AIState(owner) {}
+
+void AIAttack::update(float deltaTime) {
+    SDL_Log("Updating %s state", getName());
+}
+
+void AIAttack::onEnter() {
+    SDL_Log("Entering %s state", getName());
+}
+
+void AIAttack::onExit() {
+    SDL_Log("Exiting %s state", getName());
+}
+
+const char* AIAttack::getName() const {
+    return "Attack";
+}
