b60bc83d8d0ec5b4ae485c29a0aa4efbfcbcb962
Drahiri <kodzidg@o2.pl>
2025-05-26 09:24:14 +0200

    Add AStar search

--- Chapter04/include/search.h
+++ Chapter04/include/search.h
@@ -132,4 +132,77 @@ bool GBFS(const WeightedGraphNode* start, const WeightedGraphNode* goal, GBFSMap
     return (current == goal) ? true : false;
 }
 
+// A* search
+struct AStarScratch {
+    const WeightedEdge* parentEdge = nullptr;
+    float heuristic = 0.0f;
+    bool inOpenSet = false;
+    bool inClosedSet = false;
+    float actualFromStart = 0.0f;
+};
+
+using AStarMap = std::unordered_map<const WeightedGraphNode*, AStarScratch>;
+
+bool AStar(const WeightedGraphNode* start, const WeightedGraphNode* goal, AStarMap& outMap) {
+    std::vector<const WeightedGraphNode*> openSet;
+
+    // Set current node to start and mark in closed set
+    const WeightedGraphNode* current = start;
+    outMap[current].inClosedSet = true;
+
+    do {
+        // Add adjacent nodes to open set
+        for(const WeightedEdge* edge: current->edges) {
+            const WeightedGraphNode* neighbor = edge->to;
+
+            // Get a scratch data for this node
+            AStarScratch& data = outMap[neighbor];
+
+            // Only check nodes that aren't in the closed set
+            if(!data.inClosedSet) {
+                if(!data.inOpenSet) {
+                    // Not in the open set, so parent must by current
+                    data.parentEdge = edge;
+                    data.heuristic = computeHeuristic(neighbor, goal);
+
+                    // Actual cost is the parent's plus cost of traversing edge
+                    data.actualFromStart = outMap[current].actualFromStart + edge->weight;
+                    data.inClosedSet = true;
+                    openSet.emplace_back(neighbor);
+                } else {
+                    // Compute what new actual cost is if current becomes parent
+                    float newG = outMap[current].actualFromStart + edge->weight;
+                    if(newG < data.actualFromStart) {
+                        // Current should adopt this node
+                        data.parentEdge = edge;
+                        data.actualFromStart = newG;
+                    }
+                }
+            }
+        }
+
+        if(openSet.empty()) {
+            break;
+        }
+
+        // Find lowest cost node in open set
+        auto iter = std::min_element(openSet.begin(),
+              openSet.end(),
+              [&outMap](const WeightedGraphNode* a, const WeightedGraphNode* b) {
+                  // Calculate f(x) for nodes a/b
+                  float fOfA = outMap[a].heuristic + outMap[a].actualFromStart;
+                  float fOfB = outMap[b].heuristic + outMap[b].actualFromStart;
+                  return fOfA < fOfB;
+              });
+
+        // Set to current and move from open to closed
+        current = *iter;
+        openSet.erase(iter);
+        outMap[current].inOpenSet = false;
+        outMap[current].inClosedSet = true;
+    } while(current != goal);
+
+    return (current == goal) ? true : false;
+}
+
 #endif
