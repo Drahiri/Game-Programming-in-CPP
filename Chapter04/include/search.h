981502ebf816c4dbcad89113f1eb77a92fd14b80
Drahiri <kodzidg@o2.pl>
2025-05-26 08:09:08 +0200

    Add GBFS search

--- Chapter04/include/search.h
+++ Chapter04/include/search.h
@@ -1,6 +1,7 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include <algorithm>
 #include <queue>
 #include <unordered_map>
 #include <vector>
@@ -27,6 +28,10 @@ struct WeightedEdge {
 struct WeightedGraphNode {
     // Stores outgoing edges
     std::vector<WeightedEdge*> edges;
+};
+
+struct WeightedGraph {
+    std::vector<WeightedGraphNode*> nodes;
 };
 
 using NodeToParentMap = std::unordered_map<const GraphNode*, const GraphNode*>;
@@ -64,6 +69,67 @@ bool BFS(
     }
 
     return pathFound;
+}
+
+// GBFS search
+float computeHeuristic(const WeightedGraphNode* a, const WeightedGraphNode* b) {
+    return 0.0f;
+}
+
+struct GBFSScratch {
+    const WeightedEdge* parentEdge = nullptr;
+    float heuristic = 0.0f;
+    bool inOpenSet = false;
+    bool inClosedSet = false;
+};
+
+using GBFSMap = std::unordered_map<const WeightedGraphNode*, GBFSScratch>;
+
+bool GBFS(const WeightedGraphNode* start, const WeightedGraphNode* goal, GBFSMap& outMap) {
+    std::vector<const WeightedGraphNode*> openSet;
+
+    // Set current node to start, and mark in closed set
+    const WeightedGraphNode* current = start;
+    outMap[current].inClosedSet = true;
+
+    do {
+        // Add adjacent nodes to open set
+        for(const WeightedEdge* edge: current->edges) {
+            // Get scratch data for this node
+            GBFSScratch& data = outMap[edge->to];
+
+            // Add it only if it's not in closed set
+            if(!data.inClosedSet) {
+                // Set the adjacent node's parent edge
+                data.parentEdge = edge;
+                if(!data.inOpenSet) {
+                    // Compute the heuristic for this node, and add to open set
+                    data.heuristic = computeHeuristic(edge->to, goal);
+                    data.inOpenSet = true;
+                    openSet.emplace_back(edge->to);
+                }
+            }
+        }
+
+        if(openSet.empty()) {
+            break; // Break out of outer loop
+        }
+
+        // Find lowest cost node in open set
+        auto iter = std::min_element(openSet.begin(),
+              openSet.end(),
+              [&outMap](const WeightedGraphNode* a, const WeightedGraphNode* b) {
+                  return outMap[a].heuristic < outMap[b].heuristic;
+              });
+
+        // Set to current and move from open to closed
+        current = *iter;
+        openSet.erase(iter);
+        outMap[current].inOpenSet = false;
+        outMap[current].inClosedSet = true;
+    } while(current != goal);
+
+    return (current == goal) ? true : false;
 }
 
 #endif
