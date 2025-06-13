f5d58c68fc474b6e50bc21388a10675a8b1ad68a
Drahiri <kodzidg@o2.pl>
2025-06-13 09:09:40 +0200

    Get code from Chapter05 without game actors

---
+++ Chapter06/include/actor.h
@@ -0,0 +1,79 @@
+#ifndef ACTOR_H
+#define ACTOR_H
+
+#include "math.h"
+
+#include <vector>
+
+class Component;
+class Game;
+
+class Actor {
+public:
+    // Used to track state of actor
+    enum class State {
+        Active,
+        Paused,
+        Dead
+    };
+
+    // Constructor/destructor
+    Actor(Game* game);
+    virtual ~Actor();
+
+    // Update function called from Game (not overridable)
+    void update(float deltaTime);
+    // Updates all the components attached to the actor
+    void updateComponents(float deltaTime);
+    // Any actor-specific update code (overridable)
+    virtual void updateActor(float deltaTime);
+
+    // Getters/setters
+    State getState() const;
+    void setState(State newState);
+
+    const Vector2& getPosition() const;
+    void setPosition(const Vector2& newPosition);
+
+    float getScale() const;
+    void setScale(float newScale);
+
+    float getRotation() const;
+    void setRotation(float newRotation);
+
+    Vector2 getForward() const;
+
+    Game* getGame() const;
+
+    // Add/remove components
+    void addComponent(Component* component);
+    void removeComponent(Component* component);
+
+    // ProcessInput function called from Game (not overridable)
+    void processInput(const bool* keyState);
+
+    // Any actor-specific input code (overridable)
+    virtual void actorInput(const bool* keyState);
+
+    // Matrix transforms
+    void computeWorldTransform();
+    const Matrix4& getWorldTransform() const;
+
+private:
+    // Actor's state
+    State state;
+
+    // Transform
+    Vector2 position; // Center position of actor
+    float scale;      // Uniforms scale of actor (1.0f)
+    float rotation;   // Rotation angle (in radians)
+
+    Matrix4 worldTransform;
+    bool recomputeWorldTransform;
+
+    // Components held by this actor
+    std::vector<Component*> components;
+    Game* game;
+};
+
+#endif
---
+++ Chapter06/src/vertex_array.cpp
@@ -0,0 +1,63 @@
+#include "vertex_array.h"
+
+#include <GL/glew.h>
+
+VertexArray::VertexArray(const float* verts,
+      unsigned int numVerts,
+      const unsigned int* indices,
+      unsigned int numIndices) :
+    numVerts(numVerts),
+    numIndices(numIndices) {
+    // Create VBO
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+
+    // Create vertex buffer
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER,       // The active buffer to write to
+          numVerts * 5 * sizeof(float), // Number of bytes to copy
+          verts,                        // Source to copy from (pointer)
+          GL_STATIC_DRAW                // How will we use this data?
+    );
+
+    // Create index buffer
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,    // Index buffer
+          numIndices * sizeof(unsigned int), // Size of data
+          indices,
+          GL_STATIC_DRAW);
+
+    // Activate vertex attribute
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, // Attribute index (0 for first one)
+          3,                 // Number of components (3 in this case)
+          GL_FLOAT,          // Type of the components
+          GL_FALSE,          // (Only used for integral types)
+          sizeof(float) * 5, // Stride (usually size of each vertex)
+          0                  // offset from start of vertex to this attribute
+    );
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(
+          1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, reinterpret_cast<void*>(sizeof(float) * 3));
+}
+
+VertexArray::~VertexArray() {
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &indexBuffer);
+    glDeleteVertexArrays(1, &vertexArray);
+}
+
+void VertexArray::setActive() {
+    glBindVertexArray(vertexArray);
+}
+
+unsigned int VertexArray::getNumIndices() const {
+    return numIndices;
+}
+
+unsigned int VertexArray::getNumVerts() const {
+    return numVerts;
+}
