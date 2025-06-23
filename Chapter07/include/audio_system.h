1dd64ad7baf582aece0a9323706cec0bad578463
Drahiri <kodzidg@o2.pl>
2025-06-23 12:29:50 +0200

    Loading and unloading banks and events

--- Chapter07/include/audio_system.h
+++ Chapter07/include/audio_system.h
@@ -1,11 +1,16 @@
 #ifndef AUDIO_SYSTEM_H
 #define AUDIO_SYSTEM_H
 
+#include <string>
+#include <unordered_map>
+
 // FMOD forward declaration
 namespace FMOD {
 class System;
 
 namespace Studio {
+class Bank;
+class EventDescription;
 class System;
 } // namespace Studio
 } // namespace FMOD
@@ -21,6 +26,10 @@ public:
     void shutdown();
     void update(float deltaTime);
 
+    void loadBank(const std::string& name);
+    void unloadBank(const std::string& name);
+    void unloadAllBanks();
+
 private:
     Game* game;
     // FMOD studio system
@@ -28,6 +37,12 @@ private:
 
     // FMOD Low-lewel system (in case needed)
     FMOD::System* lowLevelSystem;
+
+    // Map of loaded banks
+    std::unordered_map<std::string, FMOD::Studio::Bank*> banks;
+
+    // Map of event name to EventDescription
+    std::unordered_map<std::string, FMOD::Studio::EventDescription*> events;
 };
 
 #endif
--- Chapter07/src/audio_system.cpp
+++ Chapter07/src/audio_system.cpp
@@ -3,6 +3,7 @@
 #include <fmod_errors.h>
 #include <fmod_studio.hpp>
 #include <SDL3/SDL_log.h>
+#include <vector>
 
 AudioSystem::AudioSystem(Game* game) : game(game), system(nullptr), lowLevelSystem(nullptr) {}
 
@@ -32,6 +33,10 @@ bool AudioSystem::initialize() {
 
     system->getCoreSystem(&lowLevelSystem);
 
+    // Load the master banks (strings first)
+    loadBank("assets/Master Bank.strings.bank");
+    loadBank("assets/Master Bank.bank");
+
     return true;
 }
 
@@ -43,4 +48,95 @@ void AudioSystem::shutdown() {
 
 void AudioSystem::update(float deltaTime) {
     system->update();
+}
+
+void AudioSystem::loadBank(const std::string& name) {
+    // Prevent double-loading
+    if(banks.find(name) != banks.end()) {
+        return;
+    }
+
+    // Try to load bank
+    FMOD::Studio::Bank* bank = nullptr;
+    FMOD_RESULT result = system->loadBankFile(name.c_str(), // File name of bank
+          FMOD_STUDIO_LOAD_BANK_NORMAL,                     // Normal loading
+          &bank                                             // Save pointer to bank
+    );
+
+    if(result != FMOD_OK) {
+        SDL_Log("Error while loading bank '%s': %s", name.c_str(), FMOD_ErrorString(result));
+        return;
+    }
+
+    const int maxPathLength = 512;
+    // Add bank to map
+    banks.emplace(name, bank);
+
+    // Load all non-streaming sample data
+    bank->loadSampleData();
+    // Get the number of events in this bank
+    int numEvents = 0;
+    bank->getEventCount(&numEvents);
+
+    if(numEvents > 0) {
+        // Get list of event descriptions in this bank
+        std::vector<FMOD::Studio::EventDescription*> eventDescs(numEvents);
+        bank->getEventList(eventDescs.data(), numEvents, &numEvents);
+        char eventName[maxPathLength];
+        for(int i = 0; i < numEvents; i++) {
+            FMOD::Studio::EventDescription* e = eventDescs[i];
+            // Get the path of this event (like event:/Explosion2D)
+            e->getPath(eventName, maxPathLength, nullptr);
+            // Add to event map
+            events.emplace(eventName, e);
+        }
+    }
+}
+
+void AudioSystem::unloadBank(const std::string& name) {
+    // Ignore if not loaded
+    auto iter = banks.find(name);
+    if(iter == banks.end()) {
+        return;
+    }
+
+    // First we nne to remove all events from this bank
+    FMOD::Studio::Bank* bank = iter->second;
+    int numEvents = 0;
+    bank->getEventCount(&numEvents);
+    if(numEvents > 0) {
+        // Get event descriptions for this bank
+        std::vector<FMOD::Studio::EventDescription*> eventDescs(numEvents);
+        // Get list of evetns
+        bank->getEventList(eventDescs.data(), numEvents, &numEvents);
+        char eventName[512];
+        for(int i = 0; i < numEvents; i++) {
+            FMOD::Studio::EventDescription* e = eventDescs[i];
+            // Get the path of this event
+            e->getPath(eventName, 512, nullptr);
+            // Remove this event
+            auto eventi = events.find(eventName);
+            if(eventi != events.end()) {
+                events.erase(eventi);
+            }
+        }
+    }
+
+    // Unload sample data and bank
+    bank->unloadSampleData();
+    bank->unload();
+
+    // Remove from banks map
+    banks.erase(iter);
+}
+
+void AudioSystem::unloadAllBanks() {
+    for(auto iter: banks) {
+        // Unload the sample data, then the bank itself
+        iter.second->unloadSampleData();
+        iter.second->unload();
+    }
+    banks.clear();
+    // No banks means no events
+    events.clear();
 }
