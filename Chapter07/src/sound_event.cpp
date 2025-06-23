26bb233bcd7a1d2e72e1c44f52eacc058a82bf08
Drahiri <kodzidg@o2.pl>
2025-06-23 13:49:51 +0200

    Add SoundEvent class

---
+++ Chapter07/src/sound_event.cpp
@@ -0,0 +1,93 @@
+#include "sound_event.h"
+
+#include "audio_system.h"
+#include "fmod_studio.hpp"
+
+SoundEvent::SoundEvent() : system(nullptr), ID(0) {}
+
+SoundEvent::SoundEvent(AudioSystem* system, unsigned int id) : system(system), ID(id) {}
+
+bool SoundEvent::isValid() {
+    return (system && system->getEventInstance(ID) != nullptr);
+}
+
+void SoundEvent::restart() {
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        event->start();
+    }
+}
+
+void SoundEvent::stop(bool allowFadeOut) {
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        FMOD_STUDIO_STOP_MODE mode =
+              allowFadeOut ? FMOD_STUDIO_STOP_ALLOWFADEOUT : FMOD_STUDIO_STOP_IMMEDIATE;
+        event->stop(mode);
+    }
+}
+
+void SoundEvent::setPaused(bool pause) {
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+
+    if(event) {
+        event->setPaused(pause);
+    }
+}
+
+void SoundEvent::setVolume(float value) {
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        event->setVolume(value);
+    }
+}
+
+void SoundEvent::setPitch(float value) {
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        event->setPitch(value);
+    }
+}
+
+void SoundEvent::setParameter(const std::string& name, float value) {
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        event->setParameterByName(name.c_str(), value);
+    }
+}
+
+bool SoundEvent::getPaused() const {
+    bool retVal = false;
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        retVal = event->getPaused(&retVal);
+    }
+    return retVal;
+}
+
+float SoundEvent::getVolume() const {
+    float retVal = false;
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        retVal = event->getVolume(&retVal);
+    }
+    return retVal;
+}
+
+float SoundEvent::getPitch() const {
+    float retVal = false;
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        retVal = event->getPitch(&retVal);
+    }
+    return retVal;
+}
+
+float SoundEvent::getParameter(const std::string& name) {
+    float retVal = false;
+    auto event = system ? system->getEventInstance(ID) : nullptr;
+    if(event) {
+        retVal = event->getParameterByName(name.c_str(), &retVal);
+    }
+    return retVal;
+}
