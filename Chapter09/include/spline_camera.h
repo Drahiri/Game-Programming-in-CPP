1aa98c23d9283abb1fbeff04ed83286466f349ac
Drahiri <kodzidg@o2.pl>
2025-07-04 13:26:03 +0200

    Add SplineCamera

--- Chapter09/include/spline_camera.h
+++ Chapter09/include/spline_camera.h
@@ -1,9 +1,12 @@
 #ifndef SPLINE_CAMERA_H
 #define SPLINE_CAMERA_H
 
+#include "camera_component.h"
 #include "math.h"
 
 #include <vector>
+
+class Actor;
 
 struct Spline {
     // Control points for spline
@@ -16,6 +19,29 @@ struct Spline {
     Vector3 compute(size_t startIdx, float t) const;
 
     size_t getNumPoints() const;
+};
+
+class SplineCamera : public CameraComponent {
+public:
+    SplineCamera(Actor* owner);
+
+    void update(float deltaTime) override;
+
+    void setSpeed(float speed);
+    void setSpline(const Spline& spline);
+    void setPaused(bool pause);
+
+private:
+    // Spline path camera follows
+    Spline path;
+    // Current control point index and t
+    size_t index;
+    float t;
+
+    // Amount t changed/sec
+    float speed;
+    // Wherther to move the camera along the path
+    bool paused;
 };
 
 #endif
--- Chapter09/src/spline_camera.cpp
+++ Chapter09/src/spline_camera.cpp
@@ -1,5 +1,7 @@
 #include "spline_camera.h"
 
+#include "actor.h"
+
 Vector3 Spline::compute(size_t startIdx, float t) const {
     // Check if startIdx is out of bounds
     if(startIdx >= controlPoints.size()) {
@@ -27,4 +29,55 @@ Vector3 Spline::compute(size_t startIdx, float t) const {
 
 size_t Spline::getNumPoints() const {
     return controlPoints.size();
+}
+
+SplineCamera::SplineCamera(Actor* owner) :
+    CameraComponent(owner),
+    index(1),
+    t(0.0f),
+    speed(0.5f),
+    paused(true) {}
+
+void SplineCamera::update(float deltaTime) {
+    CameraComponent::update(deltaTime);
+
+    // Update t value
+    if(!paused) {
+        t += speed * deltaTime;
+        // Advance to the next control point if needed.
+        // This assumes speed isn't so fast that you jump past
+        // multiple control points on one frame
+        if(t >= 1.0f) {
+            // Make sure we have enough points to advance the path
+            if(index < path.getNumPoints() - 3) {
+                index++;
+                t = t - 1.0f;
+            } else {
+                // Path's done, to pause
+                paused = true;
+            }
+        }
+    }
+
+    // Camera position is the spline at the current t/index
+    Vector3 cameraPos = path.compute(index, t);
+    // Target point is juzst a small delta aread of the spline
+    Vector3 target = path.compute(index, t + 0.01f);
+    // Assume spline doesn't flip upside down
+    const Vector3 up = Vector3::UnitZ;
+
+    Matrix4 view = Matrix4::CreateLookAt(cameraPos, target, up);
+    setViewMatrix(view);
+}
+
+void SplineCamera::setSpeed(float speed) {
+    this->speed = speed;
+}
+
+void SplineCamera::setSpline(const Spline& spline) {
+    path = spline;
+}
+
+void SplineCamera::setPaused(bool pause) {
+    paused = pause;
 }
