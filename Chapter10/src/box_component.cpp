53bc9af08451825b2596c7454c7f25f72a01fc98
Drahiri <kodzidg@o2.pl>
2025-07-11 09:31:34 +0200

    BoxComponent adds/removes itself

--- Chapter10/src/box_component.cpp
+++ Chapter10/src/box_component.cpp
@@ -1,14 +1,20 @@
 #include "box_component.h"
 
 #include "actor.h"
+#include "game.h"
+#include "phys_world.h"
 
 BoxComponent::BoxComponent(Actor* owner) :
     Component(owner),
     objectBox(Vector3::Zero, Vector3::Zero),
     worldBox(Vector3::Zero, Vector3::Zero),
-    shouldRotate(true) {}
+    shouldRotate(true) {
+    owner->getGame()->getPhysWorld()->addBox(this);
+}
 
-BoxComponent::~BoxComponent() {}
+BoxComponent::~BoxComponent() {
+    owner->getGame()->getPhysWorld()->removeBox(this);
+}
 
 void BoxComponent::onUpdateWorldTransform() {
     // Reset to object space box
