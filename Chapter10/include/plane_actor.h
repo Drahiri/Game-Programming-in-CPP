7742e1de1601741d48fc3b4dde95c4bb3462d55c
Drahiri <kodzidg@o2.pl>
2025-07-11 12:33:46 +0200

    Add BoxComponent to PlaneActor

--- Chapter10/include/plane_actor.h
+++ Chapter10/include/plane_actor.h
@@ -4,10 +4,16 @@
 #include "actor.h"
 
 class Game;
+class BoxComponent;
 
 class PlaneActor : public Actor {
 public:
     PlaneActor(Game* game);
+    ~PlaneActor();
+    BoxComponent* getBox();
+
+private:
+    BoxComponent* box;
 };
 
 #endif
--- Chapter10/src/plane_actor.cpp
+++ Chapter10/src/plane_actor.cpp
@@ -1,11 +1,22 @@
 #include "plane_actor.h"
 
+#include "box_component.h"
 #include "game.h"
+#include "mesh.h"
 #include "mesh_component.h"
 #include "renderer.h"
 
 PlaneActor::PlaneActor(Game* game) : Actor(game) {
     setScale(10.0f);
     MeshComponent* mc = new MeshComponent(this);
-    mc->setMesh(getGame()->getRenderer()->getMesh("assets/Plane.gpmesh"));
+    Mesh* mesh = getGame()->getRenderer()->getMesh("assets/Plane.gpmesh");
+    mc->setMesh(mesh);
+    BoxComponent* bc = new BoxComponent(this);
+    bc->setObjectBox(mesh->getBox());
+
+    game->addPlane(this);
+}
+
+PlaneActor::~PlaneActor() {
+    getGame()->removeActor(this);
 }
