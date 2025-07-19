52da2a72e4636754c345e20777fd885580e19ac0
Drahiri <kodzidg@o2.pl>
2025-07-19 09:56:31 +0200

    Setting mouse mode in PauseMenu

--- Chapter11/src/pause_menu.cpp
+++ Chapter11/src/pause_menu.cpp
@@ -1,14 +1,20 @@
 #include "pause_menu.h"
 
 #include "game.h"
+#include "renderer.h"
+
+#include <SDL3/SDL.h>
 
 PauseMenu::PauseMenu(Game* game) : UIScreen(game) {
     game->setState(Game::GameState::Paused);
     setTitle("PAUSED");
+
+    game->getRenderer()->setRelativeMouse(false);
 }
 
 PauseMenu::~PauseMenu() {
     game->setState(Game::GameState::Gameplay);
+    game->getRenderer()->setRelativeMouse(true);
 }
 
 void PauseMenu::handleKeyPress(int key) {
