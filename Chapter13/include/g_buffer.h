527b8f0abd9702c2eedc26635c6b281af4ad348d
Drahiri <kodzidg@o2.pl>
2025-07-30 12:22:59 +0200

    Add GBuffer class

---
+++ Chapter13/include/g_buffer.h
@@ -0,0 +1,39 @@
+#ifndef G_BUFFER_H
+#define G_BUFFER_H
+
+#include <vector>
+
+class Texture;
+
+class GBuffer {
+public:
+    // Different types of data stored in the G-buffer
+    enum class Type {
+        Diffuse = 0,
+        Normal,
+        WorldPos,
+        NUM_GBUFFER_TEXTURES
+    };
+
+    GBuffer();
+    ~GBuffer();
+
+    // Create/destroy the G-buffer
+    bool create(int width, int height);
+    void destroy();
+
+    // Get the texture for a specific type of data
+    Texture* getTexture(Type type);
+    // Get the framebuffer objectID
+    unsigned int getBufferID() const;
+    // Setup all the G-buffer textures for sampling
+    void setTexturesActive();
+
+private:
+    // Textures associated with G-buffer
+    std::vector<Texture*> textures;
+    // Framebuffer object ID
+    unsigned int bufferID;
+};
+
+#endif
