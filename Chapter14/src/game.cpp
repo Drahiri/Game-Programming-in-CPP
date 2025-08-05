b0d01e7ab8ac6ea051855555b31bc3aeba18bf7d
Drahiri <kodzidg@o2.pl>
2025-08-05 08:53:15 +0200

    Load level file for whole scene

--- Chapter14/src/game.cpp
+++ Chapter14/src/game.cpp
@@ -415,100 +415,16 @@ void Game::generateOutput() {
 void Game::loadData() {
     loadText("assets/English.gptext");
 
-    // Create actors
-    Actor* a = nullptr;
-    Quaternion q;
-
-    // Setup floor
-    const float start = -1250.0f;
-    const float size = 250.0f;
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
-            a = new PlaneActor(this);
-            Vector3 pos = Vector3(start + i * size, start + j * size, -100.0f);
-            a->setPosition(pos);
-            // Create some point lights
-            a = new Actor(this);
-            pos.z += 100.0f;
-            a->setPosition(pos);
-            PointLightComponent* p = new PointLightComponent(a);
-            Vector3 color;
-            switch((i + j) % 5) {
-            case 0:
-                color = Color::Green;
-                break;
-            case 1:
-                color = Color::Blue;
-                break;
-            case 2:
-                color = Color::Red;
-                break;
-            case 3:
-                color = Color::Yellow;
-                break;
-            case 4:
-                color = Color::LightPink;
-                break;
-
-            default:
-                break;
-            }
-            p->diffuseColor = color;
-            p->innerRadius = 100.0f;
-            p->outerRadius = 200.0f;
-        }
-    }
-
-    // Left/right walls
-    q = Quaternion(Vector3::UnitX, Math::PiOver2);
-    for(int i = 0; i < 10; i++) {
-        a = new PlaneActor(this);
-        a->setPosition(Vector3(start + i * size, start - size, 0.0f));
-        a->setRotation(q);
-
-        a = new PlaneActor(this);
-        a->setPosition(Vector3(start + i * size, -start + size, 0.0f));
-        a->setRotation(q);
-    }
-
-    // Forward/back walls
-    q = Quaternion::Concatenate(q, Quaternion(Vector3::UnitZ, Math::PiOver2));
-    for(int i = 0; i < 10; i++) {
-        a = new PlaneActor(this);
-        a->setPosition(Vector3(start - size, start + i * size, 0.0f));
-        a->setRotation(q);
-
-        a = new PlaneActor(this);
-        a->setPosition(Vector3(-start + size, start + i * size, 0.0f));
-        a->setRotation(q);
-    }
-
-    // Setup lights
-    LevelLoader::loadLevel(this, "assets/Level0.gplevel");
-
     // UI elements
     hud = new HUD(this);
+
+    // Setup lights
+    LevelLoader::loadLevel(this, "assets/Level3.gplevel");
 
     // Start music
     musicEvent = audioSystem->playEvent("event:/Music");
 
     followActor = new FollowActor(this);
-
-    // Create target actors
-    a = new TargetActor(this);
-    a->setPosition(Vector3(1450.0f, 0.0f, 100.0f));
-    a = new TargetActor(this);
-    a->setPosition(Vector3(1450.0f, 0.0f, 400.0f));
-    a = new TargetActor(this);
-    a->setPosition(Vector3(1450.0f, -500.0f, 200.0f));
-    a = new TargetActor(this);
-    a->setPosition(Vector3(1450.0f, 500.0f, 200.0f));
-    a = new TargetActor(this);
-    a->setPosition(Vector3(0.0f, -1450.0f, 200.0f));
-    a->setRotation(Quaternion(Vector3::UnitZ, Math::PiOver2));
-    a = new TargetActor(this);
-    a->setPosition(Vector3(0.0f, 1450.0f, 200.0f));
-    a->setRotation(Quaternion(Vector3::UnitZ, -Math::PiOver2));
 }
 
 void Game::unloadData() {
